4d50d8b2699f8171ffdddcd793a7577f2a97f56e
khm9888 <khm9888@naver.com>
2022-02-09 21:46:30 +0900

    1

---
+++ personal_project/2022/algorithm_baekjoon/baekjoon_단어 길이 재기_2743_b2.py
@@ -0,0 +1,21 @@
+'''
+--url--
+https://www.acmicpc.net/problem/2743
+
+--title--
+2743번: 단어 길이 재기
+
+--problem_description--
+알파벳으로만 이루어진 단어를 입력받아, 그 길이를 출력하는 프로그램을 작성하시오.
+
+--problem_input--
+첫째 줄에 영어 소문자와 대문자로만 이루어진 단어가 주어진다
+단어의 길이는 최대 100이다.
+
+--problem_output--
+첫째 줄에 입력으로 주어진 단어의 길이를 출력한다.
+
+'''
+
+word = input()
+print(len(word))
