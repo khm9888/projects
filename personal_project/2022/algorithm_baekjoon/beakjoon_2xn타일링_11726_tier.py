ea5101828452d097db2d2251650d251217663937
khm9888 <khm9888@naver.com>
2022-03-10 11:46:09 +0900

    desk

---
+++ personal_project/2022/algorithm_baekjoon/beakjoon_2xn타일링_11726_tier.py
@@ -0,0 +1,27 @@
+'''
+--url--
+https://www.acmicpc.net/problem/11726
+
+--title--
+11726번: 2×n 타일링
+
+--problem_description--
+2×n 크기의 직사각형을 1×2, 2×1 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
+아래 그림은 2×5 크기의 직사각형을 채운 한 가지 방법의 예이다.
+
+입력
+첫째 줄에 n이 주어진다. (1 ≤ n ≤ 1,000)
+
+출력
+첫째 줄에 2×n 크기의 직사각형을 채우는 방법의 수를 10,007로 나눈 나머지를 출력한다.
+'''
+
+n = int(input())
+
+answer_list=[0,1,2]
+for i in range(3,n+1):
+    answer_list.append(answer_list[i-1]+answer_list[i-2])
+
+# print(answer_list)
+print(answer_list[n]%10007)
+    
